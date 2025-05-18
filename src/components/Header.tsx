46ac3fa73224d634eedacb57b0903d3f90d835bc
[dyad] <git@dyad.sh>
2025-05-18 09:03:31 +0200

    [dyad] Mise à jour du style et des liens du Header - wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-sbf-red text-white p-4 shadow-md">
+    <header className="bg-white text-gray-800 p-4 shadow-md border-b border-gray-200"> {/* Changed background and text color, added border */}
       <div className="container mx-auto flex justify-between items-center">
-        {/* Logo Placeholder */}
-        <Link to="/" className="text-2xl font-bold text-sbf-yellow">
-          Smoothie<span className="text-white">Banana</span>Fraise
+        {/* Logo with Emojis */}
+        <Link to="/" className="text-xl font-semibold flex items-center"> {/* Adjusted text size and weight, added flex for alignment */}
+          smoothie banane fraise <span className="ml-2">🍌🍓</span> {/* Added emojis */}
         </Link>
-        {/* Navigation (placeholder for now) */}
-        <nav>
-          {/* <Link to="/programme" className="ml-4 hover:underline">Programme</Link>
-          <Link to="/blog" className="ml-4 hover:underline">Blog</Link>
-          <Link to="/chatbot" className="ml-4 hover:underline">Chatbot</Link> */}
+        {/* Navigation Links */}
+        <nav className="space-x-6"> {/* Added space between links */}
+          <Link to="/" className="hover:underline">accueil</Link>
+          <Link to="/programme" className="hover:underline">générateur de programme</Link>
+          {/* Placeholder links for other pages */}
+          <Link to="/coach-virtuel" className="hover:underline">coach virtuel</Link>
+          <Link to="/blog" className="hover:underline">blog</Link>
+          <Link to="/mon-espace" className="hover:underline">mon espace</Link>
         </nav>
       </div>
     </header>
